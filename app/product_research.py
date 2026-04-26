37d9313c21eb2c99ff2b7d473c6444245cef0488
jayjayclarke2006-sys <jayjayclarke2006@gmail.com>
2026-04-26 15:14:48 +0100

    Update product_research.py

--- app/product_research.py
+++ app/product_research.py
@@ -1,15 +1,29 @@
 def find_products():
-    return [
+    products = [
         {
             "name": "Wireless Earbuds",
             "price": 12,
-            "amazon_price": 29.99,
-            "profit": 17.99
+            "amazon_price": 29.99
         },
         {
             "name": "Phone Holder",
             "price": 3,
-            "amazon_price": 14.99,
-            "profit": 11.99
+            "amazon_price": 14.99
+        },
+        {
+            "name": "Bad Product",
+            "price": 20,
+            "amazon_price": 22
         }
     ]
+
+    profitable = []
+
+    for p in products:
+        profit = p["amazon_price"] - p["price"]
+
+        if profit > 10:
+            p["profit"] = round(profit, 2)
+            profitable.append(p)
+
+    return profitable
