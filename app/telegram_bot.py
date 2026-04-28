e1313742ba846f89ee5035417a5b1e5a1a18378e
jayjayclarke2006-sys <jayjayclarke2006@gmail.com>
2026-04-28 07:39:09 +0100

    Update telegram_bot.py

--- app/telegram_bot.py
+++ app/telegram_bot.py
@@ -1,12 +1,26 @@
 import requests
 
-# 🔥 HARDCODED (FOR TESTING — guarantees it works)
 TELEGRAM_TOKEN = "8700111978:AAHePxkgei_pvxoyTckQTkEaCf64GmnviH8"
 CHAT_ID = "5717589829"
 
 
+def send_telegram_message(text):
+    print("🚀 Sending TEXT to Telegram...")
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+
+    response = requests.post(url, data=data)
+
+    print("📩 TEXT response:", response.text)
+
+
 def send_telegram_photo(image_url, caption):
-    print("🚀 Sending to Telegram...")
+    print("🚀 Sending PHOTO to Telegram...")
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendPhoto"
 
@@ -18,4 +32,4 @@ def send_telegram_photo(image_url, caption):
 
     response = requests.post(url, data=data)
 
-    print("📩 Telegram response:", response.text)
+    print("📩 PHOTO response:", response.text)
